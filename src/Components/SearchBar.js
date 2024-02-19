38f48afe2989ad1b706234a96411f93760c1e0a7
Abraar-kanini <abraar.kanini@gmail.com>
2024-02-19 22:07:49 +0530

    Removed Everything

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -3,68 +3,59 @@ import React from 'react'
 function SearchBar() {
     return (
         <>
+            <div className="w-[95vw] h-[20vh] border border-gray rounded-xl shadow-lg shadow-gray-500/40 mt-5 m-auto flex flex-wrap gap-3">
+                <div className="w-[30%] h-[100%] border border-black self-center">
 
-
-            <div className="border border-black w-[98vw] h-[15vh] m-auto mt-6 flex flex-wrap ">
-
-
-
-
-
-
-
-
-
-
-
-                <div className="border border-black w-[25%] flex flex-col justify-center" >
-                    <h1>What Are You Looking For</h1>
-                    <div class="border border-black relative">
-                        <label for="search" class="sr-only">What Are You Looking For</label>
-                        <div class="relative">
-                            <input
-                                type="text"
-                                id="search"
-                                name="search"
-                                placeholder="What Are You Looking For"
-                                class="w-[100%] bg-gray-100  text-gray-800 font-bold  px-10 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
-                            />
-                            <span class="material-symbols-outlined absolute top-1/2 transform -translate-y-1/2 left-3 text-gray-400">search</span>
-                        </div>
+                    <h2>What Are You Looking For</h2>
+                    <div className="border border-black relative w-[100%]">
+                        <span class="material-symbols-outlined absolute">
+                            search
+                        </span>
+                        <input className="border w-[100%]  pl-6 border-black shadow-lg shadow-gray-500/40" type="text" name="" id="" />
                     </div>
-
-
-
                 </div>
-                <div className="border border-black w-[25%] flex flex-col justify-center">
 
-                    <h1>Category</h1>
-                    <select id="cars" name="cars" className="w-[100%]">
+                <div className="w-[25%] h-[100%] border border-black self-center">
+
+                    <h2>Category</h2>
+                    <select className="border border-gray-200 rounded-lg  w-[100%]" id="cars" name="cars">
                         <option value="volvo">Volvo</option>
                         <option value="saab">Saab</option>
                         <option value="fiat">Fiat</option>
                         <option value="audi">Audi</option>
                     </select>
+
+
+
                 </div>
-                <div className="border border-black w-[25%] flex flex-col justify-center">
-                    <h1>Status</h1>
-                    <select id="cars" name="cars" className="w-[100%]">
+                <div className="w-[25%] h-[100%] border border-black self-center">
+
+                    <h2>Category</h2>
+                    <select className="border border-black w-[100%]" id="cars" name="cars">
                         <option value="volvo">Volvo</option>
                         <option value="saab">Saab</option>
                         <option value="fiat">Fiat</option>
                         <option value="audi">Audi</option>
                     </select>
-                </div>
-                <div className="border border-black w-[25%] flex items-center ">
 
 
-                    <span class="material-symbols-outlined">
-                        keyboard_double_arrow_down
-                    </span> <button>Submit</button>
+
                 </div>
+
+                <div className="w-[15%] h-[100%] border border-black self-center flex items-end justify-between">
+                    <button className="border border-gray-300 h-[5vh] w-[30%] flex items-center justify-center">
+                        <span class="material-symbols-outlined">
+                            keyboard_double_arrow_down
+                        </span>
+                    </button>
+                    <button className="border border-gray-300 h-[5vh] w-[60%] flex items-center justify-center">
+                        Submit
+                    </button>
+                </div>
+
+
 
             </div>
-            
 
 
 
